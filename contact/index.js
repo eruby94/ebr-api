856856468fd282f05ce7d0881da896f57a1e371c
Evan Ruby <evanruby94@gmail.com>
2017-08-18 02:19:15 -0400

    Added working rough pass to send both emails on contact form submission. Needs refactoring

--- contact/index.js
+++ contact/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var helper = require('sendgrid').mail;
-var from_email = new helper.Email('evanruby94@gmail.com', 'Evan Ruby');
+var my_email = new helper.Email('evanruby94@gmail.com', 'Evan Ruby');
 var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 router.post('/', function (req, res) {
     var mail = new helper.Mail();
-    mail.setFrom(from_email);
+    mail.setFrom(my_email);
     var personalization = new helper.Personalization();
     personalization.addTo(new helper.Email(req.body.email));
     personalization.addSubstitution(new helper.Substitution('[%first_name%]', req.body.first));
@@ -20,12 +20,30 @@ router.post('/', function (req, res) {
 
     sg.API(request, function(error, response) {
         if (error) {
-            console.log(error);
+            console.log('error: ' + error);
+            res.send(response);
+        } else {
+            var mail2 = new helper.Mail();
+            mail2.setFrom(new helper.Email(req.body.email));
+            var personalization2 = new helper.Personalization();
+            personalization2.addTo(my_email);
+            personalization2.addSubstitution(new helper.Substitution('[%first_name%]', req.body.first));
+            personalization2.addSubstitution(new helper.Substitution('[%last_name%]', req.body.last));
+            personalization2.addSubstitution(new helper.Substitution('[%message%]', req.body.message));
+            mail2.addPersonalization(personalization2);
+            mail2.setTemplateId('4d236d5a-e9b9-411c-923e-d8b80ea3e9b6');
+            var request2 = sg.emptyRequest({
+                method: 'POST',
+                path: '/v3/mail/send',
+                body: mail2.toJSON(),
+            });
+            sg.API(request2, function(error2, response2) {
+                if (error2) {
+                    console.log('error2: ' + error2);
+                }
+                res.send(response2);
+            });
         }
-        res.send({
-            error: error,
-            response: response
-        });
     });
 });
 
