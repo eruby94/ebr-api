fcc5a2cae2a8a880dfe8f8993074f00df69d92e0
Evan Ruby <evanruby94@gmail.com>
2017-09-13 00:55:02 -0400

    Converted to prettied es6

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-var express = require('express'),
-    debug = require('debug')('app:server'),
-    logger = require('morgan'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    helmet = require('helmet'),
-    contact = require('./contact'),
-    http = require('http');
+const express = require('express'),
+	debug = require('debug')('app:server'),
+	logger = require('morgan'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	helmet = require('helmet'),
+	contact = require('./contact'),
+	http = require('http');
 
-var app = express();
+const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 
 app.use(helmet());
-app.use('/api/contact', contact);
+app.use('/contact', contact);
 
-var port = normalizePort(process.env.PORT || '3001');
+const port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
-
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 server.on('error', onError);
 server.on('listening', onListening);
 server.listen(port);
 
 function normalizePort(val) {
-    var port = parseInt(val, 10);
+	let port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
+	if (port >= 0) {
+		// port number
+		return port;
+	}
 
-    return false;
+	return false;
 }
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
 
-    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+	let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
 }
 
 function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+	let addr = server.address();
+	let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+	debug('Listening on ' + bind);
 }
